dc9575fdef6921ade4f4a7525c39c37a298aa61a
Aleksandar Ristic <storiaca@gmail.com>
2024-11-09 15:53:51 +0100

    Fetchibg data in a Page

--- next/wild-oasis-website/app/cabins/page.js
+++ next/wild-oasis-website/app/cabins/page.js
@@ -1,7 +1,18 @@
-const Cabins = () => {
+const Cabins = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+
+  const data = await res.json();
+
+  console.log(data);
+
   return (
     <div>
-      <h1>Cabins</h1>
+      <h1>Cabins page</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
