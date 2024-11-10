4c408d4db8caae2077e99806e5d3a5ef203f42d7
Aleksandar Ristic <storiaca@gmail.com>
2024-11-10 20:03:10 +0100

    Project organization

--- next/wild-oasis-website/app/cabins/page.js
+++ next/wild-oasis-website/app/cabins/page.js
@@ -1,21 +1,9 @@
-import Counter from "../components/Counter";
+import Counter from "@/app/_components/Counter";
 
 const Cabins = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-
-  const data = await res.json();
-
-  console.log(data);
-
   return (
     <div>
       <h1>Cabins page</h1>
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-      <Counter users={data} />
     </div>
   );
 };
