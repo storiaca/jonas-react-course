48dd87dbc1c850720ad8e26925c5c71b8240f562
Aleksandar Ristic <storiaca@gmail.com>
2024-12-19 17:15:37 +0100

    Finish the Date Selector done

--- next/wild-oasis-website/app/_components/DateSelector.js
+++ next/wild-oasis-website/app/_components/DateSelector.js
@@ -1,6 +1,11 @@
 "use client";
 
-import { differenceInDays, isWithinInterval } from "date-fns";
+import {
+  differenceInDays,
+  isWithinInterval,
+  isPast,
+  isSameDay,
+} from "date-fns";
 import { useContext } from "react";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
@@ -19,14 +24,14 @@ function isAlreadyBooked(range, datesArr) {
 function DateSelector({ settings, bookedDates, cabin }) {
   const { range, setRange, resetRange } = useReservation();
 
+  const displayRange = isAlreadyBooked(range, bookedDates) ? {} : range;
+
   const { regularPrice, discount } = cabin;
-  const numNights = differenceInDays(range.to, range.from);
+  const numNights = differenceInDays(displayRange.to, displayRange.from);
 
   const cabinPrice = numNights * (regularPrice - discount);
 
-  // SETTINGS
   const { minBookingLength, maxBookingLength } = settings;
-  //const maxBookingLength = 23;
 
   return (
     <div className="flex flex-col justify-between">
@@ -34,7 +39,7 @@ function DateSelector({ settings, bookedDates, cabin }) {
         className="pt-12 place-self-center"
         mode="range"
         onSelect={(range) => setRange(range)}
-        selected={range}
+        selected={displayRange}
         min={minBookingLength + 1}
         max={maxBookingLength}
         //startMonth={new Date()}
@@ -45,6 +50,10 @@ function DateSelector({ settings, bookedDates, cabin }) {
         //endMonth={new Date(year, 5)}
         captionLayout="dropdown"
         numberOfMonths={2}
+        disabled={(curDate) =>
+          isPast(curDate) ||
+          bookedDates.some((date) => isSameDay(date, curDate))
+        }
       />
 
       <div className="flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]">
